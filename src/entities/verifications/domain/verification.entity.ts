7d7b65235f89db2fc2deaa965cfc0b1a2d14d210
joseriosjose <jose_rios456@hotmail.com>
2022-10-18 22:47:01 -0500

    feat(entity): verification entity

---
+++ src/entities/verifications/domain/verification.entity.ts
@@ -0,0 +1,32 @@
+import { prop } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+import { Field, ID, ObjectType } from "type-graphql";
+import { IVerificationPoses, IVerificationStatus } from "./verification.enums";
+
+@ObjectType()
+class Verification {
+  @Field(() => ID, { description: "Verification identifier." })
+  readonly _id?: Types.ObjectId;
+
+  @Field({ description: "Image to verification." })
+  @prop({ required: true })
+  readonly media!: Types.ObjectId;
+
+  @Field({ description: "User profile to verification." })
+  @prop({ required: true })
+  readonly userId!: Types.ObjectId;
+
+  @Field({ description: "Comment of process." })
+  @prop({ required: true })
+  public detail!: string;
+
+  @Field(() => IVerificationStatus, { description: "Verification status." })
+  @prop({ required: true, enum: IVerificationStatus })
+  public status!: IVerificationStatus;
+
+  @Field(() => IVerificationPoses, { description: "Verification poses." })
+  @prop({ required: true, enum: IVerificationPoses })
+  public pose!: IVerificationPoses;
+}
+
+export default Verification;
