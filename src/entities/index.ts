29757dc7bd5b00fec25246c5bc4fd3066a1d5562
SaulMoreyra <armhacam21@gmail.com>
2022-10-24 21:51:43 -0500

    feat: implement user entity base

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { NonEmptyArray } from "type-graphql";
 import { AuthResolver } from "./auth";
+import { ChatResolver } from "./chat";
 import { DocumentResolver } from "./document";
 import { MessageResolver } from "./messages";
 import { UserResolver } from "./users";
@@ -10,6 +11,7 @@ export const resolvers: NonEmptyArray<Function> = [
   AuthResolver,
   DocumentResolver,
   MessageResolver,
+  ChatResolver,
 ];
 
 export default { resolvers };
