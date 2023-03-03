554472303ee78a57853e291c3591413e24b794d8
SaulMoreyra <armhacam21@gmail.com>
2023-03-02 18:06:01 -0600

    feat(user): implement user preference

--- src/entities/users/domain/user.entity.ts
+++ src/entities/users/domain/user.entity.ts
@@ -2,7 +2,65 @@ import Entity from "@core/domain/entity";
 import { prop } from "@typegoose/typegoose";
 import { Types } from "mongoose";
 import { Field, ID, ObjectType } from "type-graphql";
-import { IUserRole, IUserStatus } from "./user.enums";
+import {
+  IUserDegree,
+  IUserLookingFor,
+  IUserMaritalStatus,
+  IUserPersonality,
+  IUserPets,
+  IUserReligion,
+  IUserRole,
+  IUserSexualOrientation,
+  IUserStatus,
+} from "./user.enums";
+
+@ObjectType("ageRange")
+export class AgeRangePreference {
+  @Field(() => Number, { description: "User min age range" })
+  @prop({ required: false, default: 0 })
+  public min: number;
+
+  @Field(() => Number, { description: "User max age range" })
+  @prop({ required: false, default: 0 })
+  public max: number;
+}
+
+@ObjectType("preferences")
+export class UserPreference {
+  @Field(() => IUserPersonality, { description: "User personality." })
+  @prop({ required: false, enum: IUserPersonality })
+  public personality?: IUserPersonality;
+
+  @Field(() => IUserMaritalStatus, { description: "User marital status." })
+  @prop({ required: false, enum: IUserMaritalStatus })
+  public maritalStatus?: IUserMaritalStatus;
+
+  @Field(() => IUserLookingFor, { description: "Looking for." })
+  @prop({ required: false, enum: IUserLookingFor })
+  public lookingFor?: IUserLookingFor;
+
+  @Field(() => IUserPets, { description: "User pets." })
+  @prop({ required: false, enum: IUserPets })
+  public pets?: IUserPets;
+
+  @Field(() => IUserSexualOrientation, {
+    description: "User sexual orientation.",
+  })
+  @prop({ required: false, enum: IUserSexualOrientation })
+  public sexualPreference?: IUserSexualOrientation;
+
+  @Field(() => IUserDegree, { description: "User degree." })
+  @prop({ required: false, enum: IUserDegree })
+  public degree?: IUserDegree;
+
+  @Field(() => IUserReligion, { description: "User religion." })
+  @prop({ required: false, enum: IUserReligion })
+  public religion?: IUserReligion;
+
+  @Field({ description: "User age range" })
+  @prop({ required: false })
+  public ageRange?: AgeRangePreference;
+}
 
 @ObjectType()
 class User extends Entity {
@@ -26,6 +84,10 @@ class User extends Entity {
 
   @prop({ required: true })
   public password!: string;
+
+  @Field({ description: "User age range", nullable: true })
+  @prop({ required: false, default: null })
+  public preferences?: UserPreference;
 }
 
 export default User;
--- src/entities/users/infrastructure/user.inputs.ts
+++ src/entities/users/infrastructure/user.inputs.ts
@@ -1,6 +1,16 @@
-import { IsEmail, IsOptional, MaxLength, MinLength } from "class-validator";
+import { IsEmail, MaxLength, MinLength } from "class-validator";
 import { Field, InputType } from "type-graphql";
-import { IUserRole, IUserStatus } from "../domain/user.enums";
+import {
+  IUserDegree,
+  IUserLookingFor,
+  IUserMaritalStatus,
+  IUserPersonality,
+  IUserPets,
+  IUserReligion,
+  IUserRole,
+  IUserSexualOrientation,
+  IUserStatus,
+} from "../domain/user.enums";
 
 @InputType()
 export class UserInputCreate {
@@ -23,10 +33,50 @@ export class UserInputCreate {
 }
 
 @InputType()
+export class AgeRangeInputUpdatePreference {
+  @Field(() => Number, { description: "User min age range" })
+  public min: number;
+
+  @Field(() => Number, { description: "User max age range" })
+  public max: number;
+}
+
+@InputType()
+export class UserInputUpdatePreference {
+  @Field(() => IUserPersonality, { description: "User personality." })
+  public personality?: IUserPersonality;
+
+  @Field(() => IUserMaritalStatus, { description: "User marital status." })
+  public maritalStatus?: IUserMaritalStatus;
+
+  @Field(() => IUserLookingFor, { description: "Looking for." })
+  public lookingFor?: IUserLookingFor;
+
+  @Field(() => IUserPets, { description: "User pets." })
+  public pets?: IUserPets;
+
+  @Field(() => IUserSexualOrientation, {
+    description: "User sexual orientation.",
+  })
+  public sexualPreference?: IUserSexualOrientation;
+
+  @Field(() => IUserDegree, { description: "User degree." })
+  public degree?: IUserDegree;
+
+  @Field(() => IUserReligion, { description: "User religion." })
+  public religion?: IUserReligion;
+
+  @Field(() => AgeRangeInputUpdatePreference, { description: "User age range" })
+  public ageRange?: AgeRangeInputUpdatePreference;
+}
+
+@InputType()
 export class UserInputUpdate {
-  @Field({ nullable: true, description: "Name." })
-  @IsOptional()
+  @Field({ nullable: true, description: "User name" })
   @MinLength(1)
   @MaxLength(50)
   public name?: string;
+
+  @Field({ nullable: true, description: "User preferences" })
+  public preferences?: UserInputUpdatePreference;
 }
